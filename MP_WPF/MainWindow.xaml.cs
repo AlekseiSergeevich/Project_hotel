9feba8cb9f0c2e7684e775c33aaf3fbff264a010
ShchegolevRuslanSergeevich <ruslan.shchegolev2003@mail.ru>
2023-11-27 14:33:33 +0300

    пцувфвп

--- MP_WPF/MainWindow.xaml.cs
+++ MP_WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,68 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MP_WPF
+namespace ProjectMP
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        int QuantityOfDaysOfSimulation;
+        int QuantityOfRooms;
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            SettingsSimulation.Visibility = Visibility.Visible;
+            Main.Visibility = Visibility.Collapsed;
+        }
+        private void ReportsButtonClick(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Reports");
+        }
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void СontinueButtonClick(object sender, RoutedEventArgs e)
+        {
+            SettingsSimulation2.Visibility = Visibility.Visible;
+            SettingsSimulation.Visibility = Visibility.Visible;
+            QuantityOfDaysOfSimulation = int.Parse(TextBoxQuantityOfDaysOfSimulation.Text);
+            QuantityOfRooms = int.Parse(TextBoxQuantityOfRooms.Text);
+        }
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            SettingsSimulation.Visibility = Visibility.Collapsed;
+            Main.Visibility = Visibility.Visible;
+        }
+        private void Сontinue2ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Back2ButtonClick(object sender, RoutedEventArgs e)
+        {
+            SettingsSimulation.Visibility = Visibility.Visible;
+            SettingsSimulation2.Visibility = Visibility.Collapsed;
+        }
+        private void TextBoxQuantityOfDaysOfSimulation_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+        private void TextBoxQuantityOfRooms_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+        //доделать для остальных textbox и проверку на то, чтобы сумма всех номеров разных типов была равна общему количеству номеров.
     }
 }
