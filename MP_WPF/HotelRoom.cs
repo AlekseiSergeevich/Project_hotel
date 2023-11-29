f0f1bd8db8eedf8aae117f0f6f0f44480c484a83
AlekseiSergeevich <alekseisuharev03@gmail.com>
2023-11-29 17:19:23 +0300

    First Class

---
+++ MP_WPF/HotelRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP_WPF
+{
+    public abstract class HotelRoom
+    {
+        public int price;
+        public int numberOfPerson;
+        public int roomNumber;
+    }
+}
